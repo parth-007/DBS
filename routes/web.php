9faf1235f68953883cf376b1fd8e704b4b22a3ac
avkash1 <avkashgym@gmail.com>
2019-04-02 01:43:31 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes test
+| Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
